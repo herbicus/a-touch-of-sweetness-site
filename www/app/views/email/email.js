dc3c6683d9e3fd7f779ca95ce8dd900d021299ec
Herb Torres <herb.torres@byhook.com>
2016-06-08 13:51:23 -0400

    wip

--- www/app/views/email/email.js
+++ www/app/views/email/email.js
@@ -17,6 +17,29 @@ var Email = Backbone.View.extend({
     
     this.$el.html(template());
 
+    $(".l-email-container .button").click(function() {
+        var name = $("#form_name").val();
+        var email = $("#form_email").val();
+        var text = $("#msg_text").val();
+
+    		var data = {
+    		    name: $("#form_name").val(),
+    		    email: $("#form_email").val(),
+    		    message: $("#msg_text").val()
+    		};
+
+    		$.ajax({
+    		    type: "POST",
+    		    url: "email.php",
+    		    data: data,
+    		    success: function(){
+    		        console.log('success');
+    		    }
+    		});
+
+        return false;
+    });
+
   }
 
 });
