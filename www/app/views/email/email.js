a0c085bd3b42e5c78aec842e35722c42680addee
Herb Torres <herb.torres@byhook.com>
2016-09-11 12:55:47 -0400

    stashing this??

--- www/app/views/email/email.js
+++ www/app/views/email/email.js
@@ -17,24 +17,46 @@ var Email = Backbone.View.extend({
     
     this.$el.html(template());
 
+    // $("#submit").click(function() {
+    //   var name = $("#name").val();
+    //   var email = $("#email").val();
+    //   var message = $("#message").val();
+    //   var contact = $("#contact").val();
+
+    //   $("#returnmessage").empty(); // To empty previous error/success message.
+    //   // Checking for blank fields.
+
+    //   if (name == '' || email == '' || contact == '') {
+    //     alert("Please Fill Required Fields");
+    //   } else {
+    //     // Returns successful data submission message when the entered information is stored in database.
+    //     $.post("contact_form.php", {
+    //       name1: name,
+    //       email1: email,
+    //       message1: message,
+    //       contact1: contact
+    //     }, function(data) {
+    //       $("#returnmessage").append(data); // Append returned message to message paragraph.
+    //       if (data == "Your Query has been received, We will contact you soon.") {
+    //         $("#form")[0].reset(); // To reset form fields on success.
+    //       }
+    //     });
+
+    //   }
+    // });
+
     $("#submit").click(function() {
+      
       var name = $("#name").val();
       var email = $("#email").val();
       var message = $("#message").val();
-      var contact = $("#contact").val();
 
       $("#returnmessage").empty(); // To empty previous error/success message.
       // Checking for blank fields.
-
-      if (name == '' || email == '' || contact == '') {
-        alert("Please Fill Required Fields");
-      } else {
-        // Returns successful data submission message when the entered information is stored in database.
-        $.post("contact_form.php", {
-          name1: name,
-          email1: email,
-          message1: message,
-          contact1: contact
+      $.post("contact_form.php", {
+        name1: name,
+        email1: email,
+        message1: message
       }, function(data) {
         $("#returnmessage").append(data); // Append returned message to message paragraph.
         if (data == "Your Query has been received, We will contact you soon.") {
@@ -42,7 +64,7 @@ var Email = Backbone.View.extend({
         }
       });
 
-      }
+      
     });
   }
 
