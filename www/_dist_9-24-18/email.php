81d2bdc8e65dcedb5722de1b6b7a7f18ee07a020
Herb Torres <herb.torres@byhook.com>
2018-09-24 23:27:55 -0400

    adds backup compiled working build

---
+++ www/_dist_9-24-18/email.php
@@ -0,0 +1,31 @@
+<?php
+	$name = $_POST['name1'];
+	$email = $_POST['email1'];
+	$message = $_POST['message1'];
+	$phone = $_POST['phone1'];
+	$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+	
+	if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+		$subject = $name;
+
+		$headers = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		$headers .= 'From:' . $email. "\r\n"; // Sender's Email
+		$headers .= 'Cc:' . $email. "\r\n"; // Carbon copy to Sender
+		$template = '<div style="padding: 50px; color: #000; font: 300 18px/24px sans-serif;">Hello ' . $name . ',<br/>'
+		. '<br/>Thank you...! For Contacting Us.<br/><br/>'
+		. 'Name: ' . $name . '<br/>'
+		. 'Email: ' . $email . '<br/>'
+		. 'Contact No: ' . $phone . '<br/>'
+		. 'Message: ' . $message . '<br/><br/>'
+		. 'This is a Contact Confirmation mail.'
+		. '<br/>'
+		. 'We Will contact You as soon as possible .</div>';
+
+		$sendmessage = "<div style=\"background-color: #FFF; color: #000;\">" . $template . "</div>";
+		$sendmessage = wordwrap($sendmessage, 70);
+
+		mail("herbtorres960@gmail.com", $subject, $sendmessage, $headers);
+	}
+?>
